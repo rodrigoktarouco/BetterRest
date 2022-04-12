a3518cce53dbf9ce9ddda72a9efd9e1a70f3ce47
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2022-04-12 15:53:04 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rodrigo Tarouco on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
